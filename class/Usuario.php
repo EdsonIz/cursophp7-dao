09da37cebc82af02a0eb5a4ab2098e969bf1a2bf
EdsonIz <edson@imagemfolheados.com.br>
2018-09-25 14:24:29 -0300

    criamos a classe Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,8 +1,65 @@
 <?php
+class Usuario {
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+    
+    public function getIdusuario() {
+        return $this->idusuario;
+    }
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    public function getDeslogin() {
+        return $this->deslogin;
+    }
+
+    public function getDessenha() {
+        return $this->dessenha;
+    }
+
+    public function getDtcadastro() {
+        return $this->dtcadastro;
+    }
+
+    public function setIdusuario($idusuario) {
+        $this->idusuario = $idusuario;
+    }
+
+    public function setDeslogin($deslogin) {
+        $this->deslogin = $deslogin;
+    }
+
+    public function setDessenha($dessenha) {
+        $this->dessenha = $dessenha;
+    }
+
+    public function setDtcadastro($dtcadastro) {
+        $this->dtcadastro = $dtcadastro;
+    }
+
+    public function loadById($id){
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+        
+        if (count($results) > 0){
+            $row = $results[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+    }
+    
+    public function __toString() {
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "desenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:m:s")
+        ));
+    }
+}
 
